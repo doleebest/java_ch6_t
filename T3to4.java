c6d3193d3b87dc45ec188ed5870050a2a39fcad3
doleebest <90204371+doleebest@users.noreply.github.com>
2022-02-10 15:11:20 +0900

    Add files via upload

---
+++ T3to4.java
@@ -0,0 +1,40 @@
+class T3to4 { 
+	public static void main(String args[]) { 
+		Student s = new Student();
+		s.name = "홍길동";
+		s.ban = 1;
+		s.no = 1;
+		s.kor = 100;
+		s.eng = 60;
+		s.math = 76;
+		
+		System.out.println("이름="+s.name);
+		System.out.println("총점="+s.getTotal());
+		System.out.println("평균="+s.getAverage());
+	} 
+}
+
+class Student { 
+	
+	String name;
+	int ban;
+	int no;
+	int kor;
+	int eng;
+	int math;
+	
+	int getTotal() {
+		int sum = kor+eng+math;
+		return sum;
+		//간단하게 return kor+eng+math;
+	}
+	
+	float getAverage()
+	{
+//		float sum = kor+eng+math;
+//		float avg = (float)sum/3;
+//		return avg;
+// 틀린 답
+        return (int)(getTotal()/3f*10+0.5f)/10f;
+	}
+}
