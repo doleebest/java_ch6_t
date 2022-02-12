ac77da84dbf0bc3232a561414fffc6e58ab98545
doleebest <90204371+doleebest@users.noreply.github.com>
2022-02-12 14:09:00 +0900

    Add files via upload

---
+++ T24.java
@@ -0,0 +1,21 @@
+class T24
+{
+	/*
+(1) abs . 메서드를 작성하시오
+	 */
+	public static int abs(int value) {
+		if(value<0)
+			return -value;
+		else {
+			return value;
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		int value = 5;
+		System.out.println(value+"의 절대값 :"+abs(value));
+		value = -10;
+		System.out.println(value+" 의 절대값 :"+abs(value));
+	}
+}
